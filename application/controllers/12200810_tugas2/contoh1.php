9cf044946a8757089b963c525f7279e26d874fd5
riepman <91196066+riepman@users.noreply.github.com>
2021-10-02 15:37:38 +0700

    Add files via upload

---
+++ application/controllers/12200810_tugas2/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+  public function index()
+  {
+    echo "<h1>Perkenalan<h1>";
+    echo "Nama saya Ariep
+    Saya tinggal di Tasikmalaya
+    Olahraga yang saya sukai adalah Basket";
+  }
+} ?>
